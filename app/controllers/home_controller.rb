ed84ace5f1ca33d933ddf9ae13eb5d7b061f1398
Michał Smykowski <michal.smykowski03@gmail.com>
2025-11-16 22:31:37 +0100

    create users

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    return redirect_to profile_path if user_signed_in?
+    redirect_to new_user_session_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "home/index"
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +11,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
+
+  get "profile", to: "users#show", as: :profile
+
 end
