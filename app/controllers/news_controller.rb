207a47cc6c37d6020cac914ee2fae3723c0518da
Michał Smykowski <michal.smykowski03@gmail.com>
2025-11-19 14:44:17 +0100

    polish news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,5 @@
 class NewsController < ApplicationController
   def index
-    @articles = NewsApiService.new.search(query: params[:q].presence || "technology", limit: 10)
+    @articles = NewsApiService.new.search(query: params[:q].presence || "technology", limit: 10, language: "pl")
   end
 end
