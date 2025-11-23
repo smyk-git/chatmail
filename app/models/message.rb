588b1aab8421ffdc27df726576e386d6f11b471c
Michał Smykowski <michal.smykowski03@gmail.com>
2025-11-24 00:15:56 +0100

    add message analyzer

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,10 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
+  after_create do
+    MessageAnalysisJob.perform_later(self.id)
+  end
+
   validates :body, presence: true
 
   DISPLAY_COUNT = 4
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_19_170056) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_22_215336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_19_170056) do
     t.bigint "conversation_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "analysis"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
